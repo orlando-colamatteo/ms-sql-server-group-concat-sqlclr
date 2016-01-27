dcabe165f1979b772b1de28c6e951614c52a9ab8
opcthree_cp <32164949+orlando-colamatteo@users.noreply.github.com>
2016-01-27 03:34:48 +0000

    merge latest from Main

--- GroupConcat/Performance Test Scripts/Performance.1.Setup.sql
+++ GroupConcat/Performance Test Scripts/Performance.1.Setup.sql
@@ -1,3 +1,21 @@
+/*
+GROUP_CONCAT string aggregate for SQL Server - https://groupconcat.codeplex.com
+Copyright (C) 2011  Orlando Colamatteo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See http://www.gnu.org/licenses/ for a copy of the GNU General Public 
+License.
+*/
+
 USE master
 GO
 
