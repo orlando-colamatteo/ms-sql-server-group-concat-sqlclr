ce286834c983007e31430b9a4db3fe70a10a863e
opcthree_cp <32164949+orlando-colamatteo@users.noreply.github.com>
2016-01-28 05:16:23 +0000

    add unit test project

---
+++ GroupConcat.UnitTest/SqlDatabaseSetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroupConcat.UnitTest
+{
+    [TestClass()]
+    public class SqlDatabaseSetup
+    {
+
+        [AssemblyInitialize()]
+        public static void InitializeAssembly(TestContext ctx)
+        {
+            // Setup the test database based on setting in the
+            // configuration file
+            SqlDatabaseTestClass.TestService.DeployDatabaseProject();
+            SqlDatabaseTestClass.TestService.GenerateData();
+        }
+
+    }
+}
